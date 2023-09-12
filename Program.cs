c6a4ef91587dc186e7a0ec43cfbe4963a9a8211c
John Lu <johnlu2006@gmail.com>
2023-09-12 14:30:51 -0500

    Configure the web API to use EF Core

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
+using LoncotesLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+    // allows passing datetimes without time zone data 
+    AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+    // allows our api endpoints to access the database through Entity Framework Core
+    builder.Services.AddNpgsql<LoncotesLibraryDbContext>(builder.Configuration["LoncotesLibraryDbConnectionString"]);
+
+    // Set the JSON serializer options
+    builder.Services.Configure<JsonOptions>(options =>
+    {
+        options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
